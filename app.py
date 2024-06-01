eba9884da8d01862247f484c0e34caf2efbb5413
Omar Moustafa <53361295+Omar280x@users.noreply.github.com>
2024-06-01 04:59:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class FaceDetectionTransformer:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        result = DeepFace.verify(img1_path=perry_image_path, img2_path=img, enforce_detection=False, model_name="OpenFace")
+        result = DeepFace.verify(img1_path=perry_image_path, img2_path=img, enforce_detection=False, model_name="Facenet")
 
         if result['verified'] == False:
             face = result['facial_areas']["img2"]
