e5272830d85956b70ac6508d34601bd3e1f8534c
Omar Moustafa <53361295+Omar280x@users.noreply.github.com>
2024-06-03 23:39:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ webrtc_ctx = webrtc_streamer(
 
 
 if webrtc_ctx.video_transformer:
-    print(webrtc_ctx.video_transformer.perry_detected)
-    if webrtc_ctx.video_transformer.perry_detected:
+    st.success(f"{webrtc_ctx.video_transformer.perry_detected}")
+    #if webrtc_ctx.video_transformer.perry_detected:
         st.success("Perry's identity is verified, Download the file below")
         # with open("present.rar", "rb") as file:
         #     st.download_button(
